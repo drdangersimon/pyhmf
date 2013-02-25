cdd28ddffccf1c293d34ed9c587fd7dd7c7a8716
Sergey Koposov <math@fortune>
2013-02-25 10:14:19 +0000

    add flexibility to test.py

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from dohmf import *
 import numpy as np,numpy.random
 np.random.seed(1)
 config.nthreads=7
-arr, earr = get_data(npix=100, outlfrac=0.001, ncens=2, ndat=1000)
-eigs = get_firstvec(arr,earr,10).T
+ncomp = 20
+arr, earr = get_data(npix=100, outlfrac=0.01, ncens=2, ndat=1000)
+eigs = get_firstvec(arr,earr,ncomp).T
 eigs1, proj1= get_hmf(arr, earr, eigs, nit=130)
 eigs2, proj2= get_hmf_smooth(arr, earr, eigs1, nit=130)
 _,chisqs = project_only(arr,earr, eigs2, getChisq=True)
